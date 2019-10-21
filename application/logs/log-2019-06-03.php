361ffeed62f08c9ce0270479add7700fc776c1b8
Roberth Junior Morales Quiroz <robinmoralesquiroz@gmail.com>
2019-10-21 13:33:19 -0500

    Primer commit para agregar todos los archivos al control de git

---
+++ application/logs/log-2019-06-03.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2019-06-03 12:11:36 --> Severity: Warning --> mysqli::real_connect(): php_network_getaddresses: getaddrinfo failed: Host desconocido.  C:\xampp\htdocs\multix10\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-06-03 12:11:36 --> Severity: Warning --> mysqli::real_connect(): (HY000/2002): php_network_getaddresses: getaddrinfo failed: Host desconocido.  C:\xampp\htdocs\multix10\system\database\drivers\mysqli\mysqli_driver.php 201
+ERROR - 2019-06-03 12:11:36 --> Unable to connect to the database
